8bd7a3735ea2c19fba1c4fc5d0c5c21cd191eba8
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2022-05-25 15:19:08 -0300

    camel3 quarkus application boostrap app

---
+++ migration/src/test/java/com/redhat/database/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.database;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
