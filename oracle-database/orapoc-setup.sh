bf64b00ac8ebfaad4a6b82236955269bf5632211
arceojaviere <arceojaviere@yahoo.com.mx>
2022-05-30 12:13:43 -0500

    Fixing the supplemental log script

--- oracle-database/orapoc-setup.sh
+++ oracle-database/orapoc-setup.sh
@@ -33,6 +33,19 @@ oc create -f $ORAPOC_SETUP_RESOURCES/orapoc-deployment.yaml
 
 # Running the setup scripts for Debezium
 
+
+# So far, the moment the database is created and ready to execute the scripts
+# cannot be reliably determined automatically because the readiness probe
+# for the database pod has not been properly configured.
+# As a temporary measure, the script will wait for manual input and then 
+# continue executing the scripts
+
+#TODO: configure an appropriate readiness probe to detect the moment
+# the database creation process finshes so the SQLPlus scripts can be executed
+
+echo "Pausing until confirmation that the database has been created. Press any key to continue"
+read -n1
+
 ORACLE_DB_POD=$(oc get pod -lapp=oracle-19c-orapoc -ogo-template="{{(index .items 0).metadata.name}}")
 
 # Create user 'OT' with password 'Orcl1234' on the PDB Oracle container and grant CONNECT and RESOURCE rpivileges
@@ -44,14 +57,7 @@ oc exec -i $ORACLE_DB_POD -- bash -c 'sqlplus ot/Orcl1234@$ORACLE_PDB' <\
   $ORAPOC_SETUP_RESOURCES/database-scripts/create-ot-tables.sql
 
 # Set up the archivelog functionality on the CDB Oracle container
+# Create a user for Debezium to observe the archive and supplemental logs
+#Add supplemental log support for all columns in the test table
 oc exec -i $ORACLE_DB_POD -- bash -c 'sqlplus sys/$ORACLE_PWD as SYSDBA;' <\
   $ORAPOC_SETUP_RESOURCES/database-scripts/setup-archivelog.sql
-
-
-
-# --------------------------------------------------- NEED TO CHECK EXACTLY WHAT WE SHOULD BE DOING HERE ----------------------------------------------------
-# Set up tablespaces for logminer, set supplemental log configuration for the test table, create de c##dbzuser,
-# and add the approrpiate privileges to the c##dbzuser user
-
-#oc exec -i $ORACLE_DB_POD -- bash -c 'sqlplus sys/$ORACLE_PWD as SYSDBA;' <\
-#  $ORAPOC_SETUP_RESOURCES/database-scripts/setup-dbzuser-and-supplemental.sql
