00bb083badeed3ce379c0fe40f159cb5e29fcfb9
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2022-05-29 21:07:59 -0300

    debezium setup script

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ oc create -f kafka/monitoring/prometheus-install
 oc create -f kafka/monitoring/grafana-install
 
 # install kafka connect 
-oc create -f kafka/monitoring/kafka-connect-metrics.yaml
+#oc create -f kafka/monitoring/kafka-connect-metrics.yaml
 
 # install mirror maker 2
 # oc create -f kafka/monitoring/kafka-mirror-maker-2-metrics.yaml
@@ -21,4 +21,12 @@ oc create -f kafka/monitoring/kafka-connect-metrics.yaml
 oc policy add-role-to-user admin system:serviceaccount:database-migration:prometheus-server
 
 # Create Oracle Database resources
-./oracle-database/orapoc-setup.sh
+./orapoc-setup.sh
+
+# Debezium 
+oc create -f kafka/debezium/debezium-secret.yaml
+oc create -f kafka/debezium/debezium-role.yaml
+oc create -f kafka/debezium/debezium-role-binding.yaml
+oc create -f kafka/debezium/kafka-connect.yaml
+oc create -f kafka/debezium/debezium-kafka-connector.yaml
+
